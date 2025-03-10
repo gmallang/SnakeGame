ec040779ffd4742a5daf7e1af82efe9ffa5403be
gmallang <chintu13.mallangi@gmail.com>
2025-03-10 11:55:17 -0400

    Create scoreboard.py

---
+++ scoreboard.py
@@ -0,0 +1,29 @@
+from turtle import Turtle
+ALIGNMENT = "center"
+FONT = ("Arial", 24, "normal")
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.highscore = 0
+        self.goto(0, 270)
+        self.penup()
+        self.color("white")
+        self.hideturtle()
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.highscore}", align= "center", font= ("Arial", 24, "normal"))
+
+    def reset(self):
+        if self.score > self.highscore:
+            self.highscore = self.score
+        self.score = 0
+        self.update_scoreboard()
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update_scoreboard()
